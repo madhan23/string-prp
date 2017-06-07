457dbe5f661317e867220f84ff555814659e28a8
madhan23 <madhanraju23@gmail.com>
2017-06-07 10:48:59 +0530

    Create poschange.java

---
+++ poschange.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.lang.*;
+class Alpha
+{
+    public static void main(String mur[])
+    {
+        Scanner in=new Scanner(System.in);
+        String str1=in.nextLine();
+        int i,j;
+        int n1=in.nextInt();
+        int n2=in.nextInt();
+        String str2[]=str1.split(" ");
+      
+        for(i=0;i<str2.length;i++)
+        {
+            if(str2[i].length()==n1||str2[i].length()==n2)
+            {
+                char r[]=str2[i].toCharArray();
+                for(j=0;j<r.length;j++)
+                {
+                 if(r[j]!='.')
+                {
+                   char d=(char)(r[j]+1);  
+                   System.out.print(d);
+                   }
+                   else{
+                       System.out.println('.');
+                   }
+              }
+            }
+              else{
+    System.out.print(" "+str2[i]);
+                   }
+        }
+               
+            }
+        }
+      
+      
+opppppp
+hai madhan how 
+
+ibj madhan inx
+
+
+
+
